a244a74f8831fa164dd486034a63d6577a93f465
Casian <mitocarucasian@protonmail.com>
2025-11-23 17:53:02 +0100

    deleted

--- api/test.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  const supabasePassword = process.env.SUPABASE_PASSWORD || "NON TROVATA";
-
-  console.log("SUPABASE_PASSWORD:", supabasePassword); // solo in locale / log Vercel
-
-  res.status(200).json({
-    supabase_password: supabasePassword,
-  });
-}
